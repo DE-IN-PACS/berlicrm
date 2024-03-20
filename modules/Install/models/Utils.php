b13fad949ebbf74b7014552de978e6ed0d830162
fw <fw@crm-now.de>
2024-03-20 12:41:59 +0100

    fix to use the correct data types

--- modules/Install/models/Utils.php
+++ modules/Install/models/Utils.php
@@ -66,7 +66,7 @@ class Install_Utils_Model {
 			$directiveValues['file_uploads'] = 'Off';
 		if (ini_get(('output_buffering') < '4096' && ini_get('output_buffering') != '0') || stripos(ini_get('output_buffering'), 'Off') > -1)
 			$directiveValues['output_buffering'] = 'Off';
-		if (ini_get('max_execution_time') < 3600 && ini_get('max_execution_time') > 0)
+		if (ini_get('max_execution_time') < 3600)
 			$directiveValues['max_execution_time'] = ini_get('max_execution_time');
 		if (self::memoryLimitInBytes() < 536870912)
 			$directiveValues['memory_limit'] = ini_get('memory_limit');
@@ -396,7 +396,7 @@ class Install_Utils_Model {
 			$error_msg_info = getTranslatedString('MSG_LIST_REASONS', 'Install').':<br>
 					-  '.getTranslatedString('MSG_DB_PARAMETERS_INVALID', 'Install').'
 					-  '.getTranslatedString('MSG_DB_USER_NOT_AUTHORIZED', 'Install');
-		} elseif(self::isMySQL($db_type) && version_compare($mysql_server_version, '4.1', '<')) {
+		} elseif(self::isMySQL($db_type) && intval($mysql_server_version) < 4.1) {
 			$error_msg = $mysql_server_version.' -> '.getTranslatedString('ERR_INVALID_MYSQL_VERSION', 'Install');
 		} elseif($db_creation_failed) {
 			$error_msg = getTranslatedString('ERR_UNABLE_CREATE_DATABASE', 'Install').' '.$db_name;
