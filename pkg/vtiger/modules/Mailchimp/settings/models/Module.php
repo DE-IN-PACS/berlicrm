205c9729e04ed3b70128d7159fdf5fedd10ef152
fw <fw@crm-now.de>
2024-04-23 11:56:00 +0200

    fixed static function beeing static

--- pkg/vtiger/modules/Mailchimp/settings/models/Module.php
+++ pkg/vtiger/modules/Mailchimp/settings/models/Module.php
@@ -14,7 +14,7 @@ class Settings_Mailchimp_Module_Model extends Vtiger_Module_Model {
 	/**
 	 * Function to get the Subscriber type (lead or contact)
 	 */
-	public function getSubscriberType() {
+	public static function getSubscriberType() {
 		$SubscriberType = 'lead';
 		$db = PearDatabase::getInstance();
 		$query = 'SELECT newsubscribertype FROM vtiger_mailchimp_settings WHERE id=1';
