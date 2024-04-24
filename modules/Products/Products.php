fefcc759aef8024a726d10b3335378ac2e2415b3
fw <fw@crm-now.de>
2024-04-24 10:29:44 +0200

    fixed TypeError

--- modules/Products/Products.php
+++ modules/Products/Products.php
@@ -196,7 +196,7 @@ class Products extends CRMEntity {
 			{
 				$conversion_rate = $currency_details[$i]['conversionrate'];
 				$actual_conversion_rate = $product_base_conv_rate * $conversion_rate;
-				$converted_price = $actual_conversion_rate * $requestPrice;
+				$converted_price = (float)$actual_conversion_rate * (float)$requestPrice;
 
 				$log->debug("Going to save the Product - $curname currency relationship");
 
