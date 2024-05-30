17d67c2b19c5146f2518b2603ce894af23cc029c
Frank Piepiorra <fp@crm-now.de>
2024-05-30 13:47:26 +0200

    better syntax

--- modules/Calendar/views/Import.php
+++ modules/Calendar/views/Import.php
@@ -96,7 +96,7 @@ class Calendar_Import_View extends Vtiger_Import_View {
 
 			$ical = new iCal();
 			$icalActivities = $ical->iCalReader("IMPORT_".$userId);
-			is_countable($icalActivities) ? $noOfActivities = count($icalActivities) : $noOfActivities = 0;
+			$noOfActivities = is_countable($icalActivities) ? count($icalActivities) : 0;
 
 			for($i=0; $i<$noOfActivities; $i++) {
 				if($icalActivities[$i]['TYPE'] == 'VEVENT') {
