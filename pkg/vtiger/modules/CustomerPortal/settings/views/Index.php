ca8f11b33e1f2b0975ecdeb44abfc0bd9809f5fd
fw <fw@crm-now.de>
2024-09-02 08:57:03 +0200

    corrected installation CustomerPortal module

--- pkg/vtiger/modules/CustomerPortal/settings/views/Index.php
+++ pkg/vtiger/modules/CustomerPortal/settings/views/Index.php
@@ -34,7 +34,7 @@ class Settings_CustomerPortal_Index_View extends Settings_Vtiger_Index_View {
 	 * @param Vtiger_Request $request
 	 * @return <Array> - List of Vtiger_JsScript_Model instances
 	 */
-	function getHeaderScripts(Vtiger_Request $request) {
+	function getHeaderScripts(Vtiger_Request $request):array {
 		$headerScriptInstances = parent::getHeaderScripts($request);
 		$moduleName = $request->getModule();
 
