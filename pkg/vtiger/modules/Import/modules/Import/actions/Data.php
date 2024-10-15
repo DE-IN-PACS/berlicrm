379cb29a3d903d8a4f5bf3f6adf3cf0c56d57105
fr <fr@crm-now.de>
2024-10-15 08:49:51 +0200

    Fixed the module import to handle CSV files

--- pkg/vtiger/modules/Import/modules/Import/actions/Data.php
+++ pkg/vtiger/modules/Import/modules/Import/actions/Data.php
@@ -67,7 +67,7 @@ class Import_Data_Action extends Vtiger_Action_Controller {
 		$defaultValues = array();
 		if (!empty($this->defaultValues)) {
 			if(!is_array($this->defaultValues)) {
-				$this->defaultValues = json_decode($this->defaultValues);
+				$this->defaultValues = json_encode(json_decode($this->defaultValues, true));
 			}
 			if($this->defaultValues != null) {
 				$defaultValues = $this->defaultValues;
