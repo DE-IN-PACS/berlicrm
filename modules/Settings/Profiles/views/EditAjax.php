f31cd2a43e7f6b2535fc0677731f0e72f4142f2b
fr <fr@crm-now.de>
2025-04-01 09:11:06 +0200

    Added return type declarations to improve compatibility

--- modules/Settings/Profiles/views/EditAjax.php
+++ modules/Settings/Profiles/views/EditAjax.php
@@ -10,15 +10,15 @@
 
 Class Settings_Profiles_EditAjax_View extends Settings_Profiles_Edit_View {
 
-    public function preProcess(Vtiger_Request $request, $display=false) {
-        return true;
+    public function preProcess(Vtiger_Request $request, $display=false):void {
+        return;
     }
     
-    public function postProcess(Vtiger_Request $request, $display=false) {
-        return true;
+    public function postProcess(Vtiger_Request $request, $display=false):void {
+        return;
     }
     
-    public function process(Vtiger_Request $request) {
+    public function process(Vtiger_Request $request):void {
         echo $this->getContents($request);
     }
     
