fec49fa76eda582984d70133e6be9d9eea6f100f
fr <fr@crm-now.de>
2025-07-01 08:12:35 +0200

    Add helper method to register multiple Smarty plugins

--- includes/runtime/Viewer.php
+++ includes/runtime/Viewer.php
@@ -203,6 +203,29 @@ class Vtiger_Viewer extends Smarty {
 		return $instance;
 	}
 
+	/**
+	 * Registers the "vtranslate" modifier plugin (if not already registered) and optionally
+	 * additional Smarty plugins (e.g. modifiers, functions) in the current instance.
+	 *
+	 * @param array $plugins  List of plugins to register:
+	 *                        Each item should be an array with keys:
+	 *                        ['type' => 'modifier', 'name' => 'xyz', 'callback' => 'func']
+	 *                        Note: 'vtranslate' will not be overwritten if already present.
+	 */
+	public function registerSmartyPlugins(array $plugins = []) {
+		if (!isset($this->registered_plugins['modifier']['vtranslate'])) {
+			$this->registerPlugin('modifier', 'vtranslate', 'vtranslate');
+		}
+		if(!empty($plugins)) {
+			foreach ($plugins as $plugin) {
+				if($plugin['type'] === 'modifier' && $plugin['name'] === 'vtranslate') {
+					continue;
+				}
+				$this->registerPlugin($plugin['type'], $plugin['name'], $plugin['callback']);
+			}
+		}
+	}
+
 }
 
 function vtemplate_path($templateName, $moduleName='') {
