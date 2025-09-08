18dd1cc9d0315c89a2ee3dbff4f7c1d915930e62
fr <fr@crm-now.de>
2025-09-08 09:52:50 +0200

    Add XML changes and quick solution for shortcuts

--- pkg/vtiger/modules/Search/languages/de_de/Settings/Search.php
+++ pkg/vtiger/modules/Search/languages/de_de/Settings/Search.php
@@ -29,7 +29,7 @@ $languageStrings = array(
 	'LBL_SEARCH_ALL' => 'alles durchsuchen',
 	'LBL_SAVE_SETTINGS' => 'Einstellungen speichern',
 
-	'LBL_SEARCH_SETUP_DESCRIPTION' => '„Felder für Globale Suche und Suchergebnis festlegen',
+	'LBL_SEARCH_SETUP_DESCRIPTION' => 'Felder für Globale Suche und Suchergebnis festlegen',
 
 );
 
