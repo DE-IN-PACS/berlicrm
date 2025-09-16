1d20dd24cdfcc05c1e94e6d32dc10471f388c5f2
fw <fw@crm-now.de>
2025-09-16 08:23:51 +0000

    added email details when getting emails

--- data/CRMEntity.php
+++ data/CRMEntity.php
@@ -2886,11 +2886,12 @@ class CRMEntity {
     }
 	
 	function get_emails($id, $cur_tab_id, $rel_tab_id, $actions=false) {
-		$query = "SELECT vtiger_activity.*, vtiger_activitycf.*, vtiger_crmentity.*, vtiger_seactivityrel.crmid AS parent_id
+		$query = "SELECT vtiger_activity.*, vtiger_activitycf.*, vtiger_crmentity.*, vtiger_seactivityrel.crmid AS parent_id, vtiger_emaildetails.*
 				  FROM vtiger_activity
 				  INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = vtiger_activity.activityid
 				  LEFT JOIN vtiger_activitycf ON vtiger_activitycf.activityid = vtiger_activity.activityid
 				  INNER JOIN vtiger_seactivityrel ON vtiger_seactivityrel.activityid = vtiger_activity.activityid
+				  LEFT JOIN vtiger_emaildetails ON vtiger_emaildetails.emailid = vtiger_crmentity.crmid
 				  WHERE vtiger_crmentity.deleted = 0 AND vtiger_seactivityrel.crmid = '$id' AND vtiger_activity.activitytype = 'Emails'";
 
 		return array('query' => $query);
